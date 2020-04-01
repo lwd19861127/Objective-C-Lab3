0f99d8b0d9a4f74f1ec98cc2b97149520022a750
WendaLi <lwd19861127@hotmail.com>
2020-04-01 21:37:40 +0800

    Initial Commit

---
+++ Lab3_Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab3_Maths
+//
+//  Created by WendaLi on 2020-04-01.
+//  Copyright © 2020 WendaLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
